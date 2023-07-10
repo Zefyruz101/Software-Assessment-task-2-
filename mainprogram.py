5102de73d1254326c157f92e8083304606adfb96
Zefyruz101 <mdfaris2006@outlook.com>
2023-07-10 15:29:36 +1000

    Update mainprogram.py

--- mainprogram.py
+++ mainprogram.py
@@ -6,3 +6,13 @@ root.title("Summing Series")
 
 #Labels and Entry Widgets
 first_term = Label(root, text="First Term: ")
+first_term.pack()
+first_term_entry = Entry(root)
+first_term_entry.pack()
+
+common_difference_label = Label(root, text="Common Difference:")
+common_difference_entry = Entry(root)
+
+num_terms_label = Label(root, text="Number of Terms:")
+num_terms_entry = Entry(root)
+root.mainloop()
