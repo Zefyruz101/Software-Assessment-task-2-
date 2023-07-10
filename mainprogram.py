1870a735368a53db377cab4aee51aec913e68ed2
devguarv <dev.prakash@education.nsw.gov.au>
2023-07-10 16:24:40 +1000

    Update mainprogram.py

--- mainprogram.py
+++ mainprogram.py
@@ -66,6 +66,7 @@ my_menu = Menu(root)
 root.config(menu=my_menu)
 
 file_menu = Menu(my_menu)
+my_menu.add_cascade(label="File", menu=file_menu)
 file_menu.add_command(label="Exit", command=root.destroy)
 
 #Loops the program
