67191b750f78afb641438b6470019b4706c3443e
devguarv <dev.prakash@education.nsw.gov.au>
2023-07-10 15:47:45 +1000

    Update mainprogram.py

--- mainprogram.py
+++ mainprogram.py
@@ -1,5 +1,11 @@
 from tkinter import *
 
+def calculate_sum():
+    pass
+
+def clear():
+    pass
+
 #Main Window
 root = Tk()
 root.title("Summing Series")
@@ -10,9 +16,34 @@ first_term.pack()
 first_term_entry = Entry(root)
 first_term_entry.pack()
 
-common_difference_label = Label(root, text="Common Difference:")
+common_difference = Label(root, text="Common Difference/Ratio:")
+common_difference.pack()
 common_difference_entry = Entry(root)
+common_difference_entry.pack()
 
-num_terms_label = Label(root, text="Number of Terms:")
+num_terms = Label(root, text="Number of Terms:")
+num_terms.pack()
 num_terms_entry = Entry(root)
+num_terms_entry.pack()
+
+#Radio Buttons
+var = IntVar()
+arithmetic_button = Radiobutton(root, variable=var, value=1, text="Arithmetic Series")
+arithmetic_button.pack()
+
+geometric_button = Radiobutton(root, variable=var, value=2, text="Geometric Series")
+geometric_button.pack()
+
+#Output Widgets
+sum_label = Label(root, text = "Sum: ")
+sum_label.pack()
+
+#Calculate and Clear Buttons
+calculate_button = Button(root, text="Calculate", command=calculate_sum)
+calculate_button.pack()
+
+clear_button = Button(root, text="Clear", command=clear)
+clear_button.pack()
+
+
 root.mainloop()
